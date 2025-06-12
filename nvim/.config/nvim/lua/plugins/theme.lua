6a4265f55103351ed27a106f3acf8db8dee845d6
Elang Aditya Rahman <elangadityar@gmail.com>
2025-06-12 13:31:01 +0700

    update to nvim 0.11.2 - change cmp to blink.cmp - remove setup from lspconfig since cause nil issue

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,26 +1,36 @@
 return {
-  -- {
-  -- 	"eldritch-theme/eldritch.nvim",
-  -- 	lazy = false,
-  -- 	priority = 1000,
-  -- 	opts = {},
-  -- 	config = function()
-  -- 		vim.cmd.colorscheme("eldritch")
-  -- 	end,
-  -- },
-  -- {
-  -- 	"folke/tokyonight.nvim",
-  -- 	lazy = false,
-  -- 	priority = 1000,
-  -- 	opts = {},
-  -- },
-  {
-    "catppuccin/nvim",
-    lazy = false,
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme("catppuccin-mocha")
-    end,
-  },
+	-- {
+	-- 	"eldritch-theme/eldritch.nvim",
+	-- 	lazy = false,
+	-- 	priority = 1000,
+	-- 	opts = {},
+	-- 	config = function()
+	-- 		vim.cmd.colorscheme("eldritch")
+	-- 	end,
+	-- },
+	-- {
+	-- 	"folke/tokyonight.nvim",
+	-- 	lazy = false,
+	-- 	priority = 1000,
+	-- 	opts = {},
+	-- },
+	{
+		"catppuccin/nvim",
+		lazy = false,
+		name = "catppuccin",
+		priority = 1000,
+		custom_highlights = function(colors)
+			return {
+				-- Comment = { fg = colors.flamingo },
+				-- TabLineSel = { bg = colors.pink },
+				-- CmpBorder = { fg = colors.surface2 },
+				-- Pmenu = { bg = colors.none },
+				BlinkCmpMenu = { bg = colors.surface0 },
+				BlinkCmpDoc = { bg = colors.surface1 },
+			}
+		end,
+		config = function()
+			vim.cmd.colorscheme("catppuccin-mocha")
+		end,
+	},
 }
